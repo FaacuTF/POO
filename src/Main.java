71243958c7640bfee2a664419dd5bfad3e4dc6b4
Marcela Artolo <artola.marcela@bue.edu.ar>
2024-05-31 11:58:19 -0300

    solucion ejercicio 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main s{
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Introduci un dia de la semana ");
+        String diadesemana = scanner.next();
+        String materia;
+
+        case "Lunes"
+        materia = "Ingles";
+
+        case "Martes"
+        materia = "Matematica";
+
+        case "Miercoles"
+        materia = "Lengua y Literatura";
+
+        case "Jueves"
+        materia = "Economia";
+
+        case "Viernes"
+        materia = "Contabilidad";
+
+     System.out.printf("En el dia de la semana" + diadesemana + "toca la materia" + materia +);
+
     }
 }
